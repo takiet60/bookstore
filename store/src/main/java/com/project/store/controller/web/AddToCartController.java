b1d35086c186cc4f80c6ac490d87e8b260c1c3fc
Gia Nguyen IT <gianguyen0622@gmail.com>
2021-01-26 21:00:46 +0700

    Create AddToCartController.java

---
+++ store/src/main/java/com/project/store/controller/web/AddToCartController.java
@@ -0,0 +1,54 @@
+package com.project.store.controller.web;
+
+import com.project.store.model.CartModel;
+import com.project.store.model.ItemModel;
+import com.project.store.model.ProductModel;
+import com.project.store.service.IProductService;
+
+import javax.inject.Inject;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.util.Collection;
+
+@WebServlet(name = "AddToCartController)", urlPatterns = "/cart/add")
+public class AddToCartController extends HttpServlet {
+
+    @Inject
+    private IProductService productService;
+
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        Long id = Long.parseLong(request.getParameter("id"));
+        if(id == null) response.sendRedirect(request.getContextPath() + "/home");
+        ProductModel productModel = productService.findById(id);
+        if(productModel == null){
+            response.sendRedirect(request.getContextPath() + "/home");
+            return;
+
+        } else{
+            HttpSession session = request.getSession();
+            CartModel cart = CartModel.getCart(session);
+            ItemModel itemModel = new ItemModel();
+            itemModel.setProductModel(productModel);
+            cart.put(itemModel);
+            cart.commit(session);
+            long total = cart.total();
+            request.setAttribute("total", total);
+            response.sendRedirect(request.getContextPath() + "/home");
+        }
+
+
+
+    }
+}
+
